925140c3e399a84d4903e51b1ae0de0cbf3b2670
JAntonioVR <juanantoniovr98@gmail.com>
2021-10-10 12:24:56 +0200

    closes josevilchez247/Odyssey#17

---
+++ src/fuel_station.py
@@ -0,0 +1,20 @@
+#
+# ────────────────────────────────────────────────────────────────────
+#   :::::: fuel_station.py : :  :   :    :     :        :          :
+# ────────────────────────────────────────────────────────────────────
+#
+
+# ─── PROYECT: Odyssey (https://github.com/josevilchez247/Odyssey) ───────────────
+# ─── OWNER: @josevilchez247 ─────────────────────────────────────────────────────
+# ─── AUTHOR: @jantoniovr ────────────────────────────────────────────────────────
+# ─── DATE: 8-10-2021 ────────────────────────────────────────────────────────────
+# ─── VERSION: 1.0 ───────────────────────────────────────────────────────────────
+
+from enum import Enum
+
+
+# Enumerador que define los distintos tipos de gasolina
+class FuelType(Enum):
+    DIESEL = "diesel"
+    DIESEL_PLUS = "diesel_plus"
+    GASOLINA = "gasolina"
